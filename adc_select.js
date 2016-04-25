9e8b0690e7d61df828617133c44b569f3b10a7c1
crecket <crecketgaming@gmail.com>
2016-04-25 12:20:58 +0200

    dynamic support image selectors

--- adc_select.js
+++ adc_select.js
@@ -1,55 +1,43 @@
-$(document).ready(function(){
-    $("#blitzcrank").click(function(){
-        $("#thumbs1 img").removeClass("border-highlight");
-        $("#blitzcrank").addClass("border-highlight");
-    });
-     $("#soraka").click(function(){
-        $("#thumbs1 img").removeClass("border-highlight");
-        $("#soraka").addClass("border-highlight");
-    });
-      $("#zyra").click(function(){
-        $("#thumbs1 img").removeClass("border-highlight");
-        $("#zyra").addClass("border-highlight");
-    });
-    $("#sona").click(function(){
-        $("#thumbs2 img").removeClass("border-highlight");
-        $("#sona").addClass("border-highlight");
-    });
-     $("#leona").click(function(){
-        $("#thumbs2 img").removeClass("border-highlight");
-        $("#leona").addClass("border-highlight");
-    });
-      $("#lux").click(function(){
-        $("#thumbs2 img").removeClass("border-highlight");
-        $("#lux").addClass("border-highlight");
-    });
-    $("#morgana").click(function(){
-        $("#thumbs3 img").removeClass("border-highlight");
-        $("#morgana").addClass("border-highlight");
-    });
-     $("#taric").click(function(){
-        $("#thumbs3 img").removeClass("border-highlight");
-        $("#taric").addClass("border-highlight");
-    });
-      $("#nautilus").click(function(){
-        $("#thumbs3 img").removeClass("border-highlight");
-        $("#nautilus").addClass("border-highlight");
+$(document).ready(function () {
+
+    // Listen for any click event on a .support_selector_image class
+    $(".support_selector_image").on('click', function () {
+
+        // Reset all highlights
+        $('.support_selector_image').removeClass("border-highlight");
+
+        // Only set the border-highlight for the current clicked element which you can simply access through 'this'
+        $(this).addClass("border-highlight");
+
+        // Now we find the target hidden input field
+        var inputTarget = $(this).data('target'); // gets value from the data-target field
+
+        // Get the current clicked element's name attribute
+        var selectedSupport = $(this).attr('name');
+
+        // Next store the value in the hidden input field
+        $(inputTarget).val(selectedSupport);
+
+        // Debugging
+        console.log(inputTarget);
+        console.log(selectedSupport);
+        console.log($(inputTarget).val());
     });
 });
-$(document).ready(function(){
-    $("#showforward1").click(function(){
+$(document).ready(function () {
+    $("#showforward1").click(function () {
         $("#section1").slideUp();
         $("#section2").slideDown();
     });
-    $("#showback1").click(function(){
+    $("#showback1").click(function () {
         $("#section2").slideUp();
         $("#section1").slideDown();
     });
-    $("#showforward2").click(function(){
+    $("#showforward2").click(function () {
         $("#section2").slideUp();
         $("#section3").slideDown();
     });
-    $("#showback2").click(function(){
+    $("#showback2").click(function () {
         $("#section3").slideUp();
         $("#section2").slideDown();
     });
